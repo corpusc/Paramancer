89403e5d88e3cd7c60a450cb04dfed99198567cd
corpusc <corpus_callosum@hotmail.com>
2014-01-11 00:27:30 -0500

    key cap texture &  mouseover

---
+++ Assets/_Scripts/Hud/KeyData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyData {
+	public KeyCode KeyCode;
+	public string Text;
+	public Rect Rect;
+}
---
+++ Assets/_Scripts/Player/UserAction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserAction {
+	enum Action {
+		MoveForward,
+		MoveBackward,
+		MoveLeft,
+		MoveRight,
+		MoveUp,
+		MoveDown,
+	}
+}
