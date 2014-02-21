275e43cbcc266da5b1781453e2150c487903247c
corpusc <corpus_callosum@hotmail.com>
2014-02-21 09:28:27 -0500

    pickup boxes changed to gun models

--- Assets/Scripts/Item/PickupBoxScript.cs
+++ Assets/Scripts/Item/PickupBoxScript.cs
@@ -11,13 +11,19 @@ public class PickupBoxScript : MonoBehaviour {
 	CcNet net;
 	GameObject localPlayer;
 	float sinny = 0f;
+	float rotationOffset; // so that they aren't all pointed in the same direction
+	Vector3 start;
 	
 	
 	
 	void Start() {
 		net = GameObject.Find("Main Program").GetComponent<CcNet>();
 		sinny = Random.Range(0f, 4f);
-		boxObj.transform.Rotate(0, 0, Random.Range(0f, 360f));
+		rotationOffset = Random.Range(0f, 360f);
+		//boxObj.transform.Rotate(0, 0, Random.Range(0f, 360f));        original setting for all BOX models
+		boxObj.transform.Rotate(270, 0, rotationOffset);
+		boxObj.transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
+		start = boxObj.transform.position + Vector3.up/3;
 	}
 	
 	void Update() {
@@ -37,8 +43,9 @@ public class PickupBoxScript : MonoBehaviour {
 		}
 		
 		sinny += Time.deltaTime * 2f;
-		boxObj.transform.position = transform.position + (Vector3.up * ((Mathf.Sin(sinny)*0.1f) + 0.3f));
-		boxObj.transform.Rotate(0,0,10f * Time.deltaTime);
+		boxObj.transform.position = start + (Vector3.up * ((Mathf.Sin(sinny)*0.2f) + 0.3f));
+//		boxObj.transform.position = transform.position + (Vector3.up * ((Mathf.Sin(sinny)*0.1f) + 0.3f));      original setting for all BOX models
+		boxObj.transform.Rotate(0, 0, 130f * Time.deltaTime);
 	}
 	
 	public void Pickup() {
