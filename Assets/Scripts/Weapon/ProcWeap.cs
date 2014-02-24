3ae8f755a94deb43ba0d8ab4c5073c95be3839b7
IceFlameCode <jalokim1998@poczta.onet.pl>
2014-02-24 20:58:43 +0100

    Tower, basic procweap class
    the box collider is there on the Tower because Unity hasn't got any good
    cylinder colliders - just capsule ones

---
+++ Assets/Scripts/Weapon/ProcWeap.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProcWeap : MonoBehaviour {
+
+	public float bps = 4f;
+	public int dmg = 10;
+	public bool auto = true;
+	public int bnc = 1;
+	public int proj = 2;
+	public float vamp = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	float GetPower(){
+		return bps * dmg * Mathf.Sqrt(bnc + 1) * proj * (auto && bps > 1.0f ? Mathf.Sqrt(bps) : 1) * (vamp + 1.0f);
+	}
+}
