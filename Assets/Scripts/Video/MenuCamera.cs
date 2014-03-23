81b5f7d234eec7162bd29be637bd04fe4f882e5d
IceFlameCode <jalokim1998@poczta.onet.pl>
2014-03-23 21:56:01 +0100

    A lot of stuff
    Renamed all announcements, better-looking main menu with moving camera
    and a high-res sci-fi font, sprinting is more intuitive - you stop
    sprinting if you don't move for 0.3 seconds, you can start sprinting
    when you're not moving, re-made some announcements for all to sound
    human... I think that's all :)

---
+++ Assets/Scripts/Video/MenuCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuCamera : MonoBehaviour {
+
+	float currentAngle = 180f;
+	float moveSpeed = 30f;
+	float height = 2f;
+	Vector3 lookAtOffset;
+	float lookAtSpeed = 0.13f;
+	float maxOffset = 2f;
+	public float DistanceFromChar = 5f;
+
+	// Use this for initialization
+	void Start () {
+		lookAtOffset = new Vector3(0f, 0f, 0f);
+		transform.position = new Vector3(Mathf.Sin(Mathf.Deg2Rad * currentAngle) * DistanceFromChar, height, Mathf.Cos(Mathf.Deg2Rad * currentAngle) * DistanceFromChar);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentAngle += moveSpeed * Time.deltaTime;
+		lookAtOffset = new Vector3(Mathf.Sin(Mathf.Deg2Rad * currentAngle * lookAtSpeed), Mathf.PingPong(currentAngle * 0.003f, 0.5f), Mathf.Sin(Mathf.Deg2Rad * currentAngle * lookAtSpeed)) * maxOffset;
+		transform.position = new Vector3(Mathf.Sin(Mathf.Deg2Rad * currentAngle) * DistanceFromChar, height, Mathf.Cos(Mathf.Deg2Rad * currentAngle) * DistanceFromChar);
+		transform.LookAt(Vector3.up * height + lookAtOffset);
+	}
+}
