8c879c121d2da23363b83df1dff402aafb6d07db
IceFlameCode <jalokim1998@poczta.onet.pl>
2014-03-30 15:55:53 +0200

    Added a test for roguelike
    It doesn't work :)

---
+++ Assets/Test/RlikePlane.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RlikePlane : MonoBehaviour {
+
+	RoguelikeLevel lev;
+
+	// Use this for initialization
+	void Start () {
+		lev = (RoguelikeLevel)this.GetComponent("RoguelikeLevel");
+		lev.MapSize.x = 128;
+		lev.MapSize.y = 128;
+		lev.Forms = 1;
+		lev.Block = new bool[128, 128];
+		lev.Build();
+		/*
+		Vec2i a;
+		Vec2i b;
+		a.x = 0;
+		a.y = 0;
+		b.x = 13;
+		b.y = 16;
+		lev.fillRect(a, b);
+		*/
+		Texture2D texture = new Texture2D(128, 128);
+		renderer.material.mainTexture = texture;
+		int y = 0;
+		while (y < texture.height) {
+			int x = 0;
+			while (x < texture.width) {
+				Color color = (lev.Block[x, y] ? Color.white : Color.gray);
+				texture.SetPixel(x, y, color);
+				++x;
+			}
+			++y;
+		}
+		texture.Apply();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
