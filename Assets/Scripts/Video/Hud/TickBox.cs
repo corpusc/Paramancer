bdcb50d86c63eec2e08ab6491f12ce76031a26a6
IceFlameCode <jalokim1998@poczta.onet.pl>
2014-04-14 22:10:21 +0200

    Added tickbox class
    not implemented yet, but should work... it will use the blue button skin
    for hovering over instead of one of its own

---
+++ Assets/Scripts/Video/Hud/TickBox.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TickBox : MonoBehaviour {
+
+	Texture2D off;
+	Texture2D on;
+
+	bool Display(bool state) {
+		off = (Texture2D)Pics.Get("Tickbox");
+		on = (Texture2D)Pics.Get("TickboxTicked");
+		return GUILayout.Button(state ? on : off);
+	}
+}
