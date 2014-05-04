5766a5307e630060ccdab25bc90419268655085f
corpusc <corpus_callosum@hotmail.com>
2014-05-04 13:57:49 -0400

    BUILD editor style map drawing prototype...
    .... you draw on the 2D map plane of the roguelike generator FOR NOW

--- Assets/Map/ProcGen/RlikePlane.cs
+++ Assets/Map/ProcGen/RlikePlane.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+
 
 public class RlikePlane : MonoBehaviour {
-
 	RoguelikeLevel lev;
 
-	// Use this for initialization
+
+
 	void Start () {
 		lev = ScriptableObject.CreateInstance<RoguelikeLevel>();
 		lev.MapSize.x = 64;
@@ -32,9 +35,58 @@ public class RlikePlane : MonoBehaviour {
 			lev.Scale.z);
 		lev.Build3D();
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
+
+
+
+
+
+	// UPDATE is SOLELY an experimental/prototype of a BUILD-like Editor 
+	public class Wall {
+		public Vector3 Pos;
+		public Wall Next;
+
+		public Wall(Vector3 pos, Wall next) {
+			Pos = pos;
+			Next = next;
+		}
+	}
+	Wall prevWall;
+	List<Wall> walls = new List<Wall>();
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Mouse0)) {
+			prevWall = maybeMakeBUILDStyleWall();
+		}
+		if (Input.GetKeyUp(KeyCode.Mouse0)) {
+			var nw = maybeMakeBUILDStyleWall();
+
+			if (prevWall != null) {
+				var nq = GameObject.CreatePrimitive(PrimitiveType.Quad);
+				var delta = nw.Pos - prevWall.Pos;
+				nq.transform.position = prevWall.Pos + delta/2;
+				nq.transform.right = delta;
+				var f = 3f;
+				nq.transform.localScale = new Vector3 (delta.magnitude, f, f);
+				prevWall = null;	
+			}
+		}
+	}
+
+	Wall maybeMakeBUILDStyleWall() {
+		var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit rh;
+		float distance = 100f;
+		if (collider.Raycast (ray, out rh, distance)) {
+			Debug.Log ("hit plane");
+			var nc = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			var f = .1f;
+			nc.transform.localScale = new Vector3 (f, f, f);
+			nc.transform.position = rh.point;
+			var nw = new Wall(rh.point, prevWall);
+			walls.Add(nw);
+			return nw;
+		}
+
+		return null;
 	}
 }
