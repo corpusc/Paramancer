0a879b78f857f4a418cbff4e75e9f8a8cc01d5dc
IceFlameCode <jalokim1998@poczta.onet.pl>
2014-05-22 17:35:29 +0200

    Neural net programmed
    still needs training system to work. Has not been debugged yet since
    this needs a training, spawning and input system.

---
+++ Assets/Scripts/Game/AI/PseudoNeuron.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PseudoNeuron : MonoBehaviour {
+	// this is a neuron in the input/output layer
+	public float Value = 0f;
+	public List<Axon> InAxons;
+	public List<Axon> OutAxons;
+	
+	public void ProcessOutput () {
+		for (int i = 0; i < OutAxons.Count; i++) {
+			OutAxons[i].Weight = Value;
+			OutAxons[i].Active = true;
+		}
+	}
+
+	public void ProcessInput () {
+		float t = 0f; // temporary value
+		for (int i = 0; i < InAxons.Count; i++) {
+			if (InAxons[i].Active) {
+				t += InAxons[i].Weight;
+			}
+		}
+		for (int i = 0; i < InAxons.Count; i++) {
+			if (InAxons[i].Active) {
+				t *= InAxons[i].Mult;
+			}
+		}
+		Value = t;
+	}
+
+	// will randomly change the input axon weights
+	public void Mutate (float MaxWeightMutation, float MaxMultMutation) {
+		for (int i = 0; i < InAxons.Count; i++) {
+			InAxons[i].Weight += Random.Range(-MaxWeightMutation, MaxWeightMutation);
+			InAxons[i].Mult += Random.Range(-MaxMultMutation, MaxMultMutation);
+		}
+	}
+
+}
