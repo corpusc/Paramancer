8d1c3147c4c9922a3ebe160c17faa8777019bb20
corpusc <corpus_callosum@hotmail.com>
2014-05-25 14:40:00 -0400

    ---

--- Assets/Scripts/Video/Hud/MessageOfTheMoment.cs
+++ Assets/Scripts/Video/Hud/MessageOfTheMoment.cs
@@ -11,9 +11,9 @@ public class MessageOfTheMoment {
 	
 	string[] tipText = { // less than 2 entries would cause endless loop 
 		"This is PURE OLD SCHOOL ARENA ACTION!  Don't ask for MODERN/MILITARY FPS features!",
-		"TIP: In BYOG matches, use the Gravulator often to confuse your enemies!",
-		"TIP: Download the standalone version for better performance!",
-		"TIP: Sometimes offense is the best defense!",
+		"TIP: In BYOG matches, use the Gravulator often, to confuse your enemies",
+		"TIP: Download the stand-alone version for better performance",
+		"TIP: Sometimes offense is the best defense",
 
 		"TIP: If you like a generated map, you can use its seed to play on it whenever you want!\n" +
 		"Do this by going into the match settings and copying the seed."
@@ -22,8 +22,8 @@ public class MessageOfTheMoment {
 
 
 	public void Draw(Hud hud) {
-		if /* time for next tip */ (Time.time > nextTip) {
-			nextTip = Time.time + msgDura;
+		if (Time.time > nextTip) {
+			nextTip += msgDura;
 
 			string newtip = "";
 
