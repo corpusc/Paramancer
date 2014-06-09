d7ee9d57417ecf6394a0c82e10d5678e781cba2a
corpusc <corpus_callosum@hotmail.com>
2014-06-09 13:20:41 -0400

    reverted & improved GrenadeExplosion

--- Assets/Resources/Item/Weap/FX/End/GrenadeExplosion.cs
+++ Assets/Resources/Item/Weap/FX/End/GrenadeExplosion.cs
@@ -1,33 +1,72 @@
 using UnityEngine;
 using System.Collections;
 
+
+
+// an explosion sphere graphic 
 public class GrenadeExplosion : MonoBehaviour {
-	public float MaxExpand;
-	public float ExpandSpeed;
+	public float MaxSize = 5f;
+	public bool IsRootSphere = true; // only the root/primary sphere should have a light, all other layers/stacks will be lit from it 
 
-
+	// private 
+	bool expanding = true;
+	float startSize = 0.2f;
 	Vector3 oriSpeed;
-	float cExpand = 0f; // current expand val
-	float f = 0.2f;
+	float expandSpeed = 35f;
+	float shrinkSpeed = 20f;
 
 
 	void Start() {
-		transform.localScale = new Vector3(f,f,f);
-		// orientation/rotation speeds 
-		oriSpeed = new Vector3(rand(), rand(), rand());
+		transform.localScale = new Vector3(startSize, startSize, startSize);
 
-		MaxExpand = Random.Range(8f, 12f);
-		ExpandSpeed = Random.Range(20f, 40f);
+		// random orientation/rotation speeds 
+		int rnd = Random.Range(0, 3);
+		switch (rnd) {
+			case 0:
+				oriSpeed = new Vector3(0f, rand(), rand());
+				break;
+			case 1:
+				oriSpeed = new Vector3(rand(), 0f, rand());
+				break;
+			case 2:
+				oriSpeed = new Vector3(rand(), rand(), 0f);
+				break;
+		}
+
+		if (!IsRootSphere)
+			light.enabled = false;
 	}
 
 	void Update() {
-		// expanding
-		cExpand += Time.deltaTime * ExpandSpeed;
-		transform.localScale = new Vector3(f,f,f) * cExpand * (MaxExpand - cExpand); // gaussian-like distribution
+		// scaling 
+			
+		var ls = transform.localScale;
+		
+		if (expanding) {
+			var f = Time.deltaTime * expandSpeed;
+			ls.x += f;
+			ls.y += f;
+			ls.z += f;
+
+			if (transform.localScale.x > MaxSize)
+				expanding = false;
+		}else{ // ...shrinking 
+			var f = Time.deltaTime * shrinkSpeed;
+			ls.x -= f;
+			ls.y -= f;
+			ls.z -= f;
+		}
+		
+		transform.localScale = ls;
 
 		// rotation & light 
-		if (light != null)
-			light.range = transform.localScale.x * 3f;
+		if (IsRootSphere) {
+			if (light != null) {
+				light.range = transform.localScale.x * 4f;
+			}
+		}
+
+		// spinning/rotation 
 		transform.eulerAngles += oriSpeed;
 
 		// cleanup 
@@ -36,8 +75,8 @@ public class GrenadeExplosion : MonoBehaviour {
 	}
 	
 	float rand() {
-		var min = 6f;
-		var max = 11f;
+		var min = 12f;
+		var max = 24f;
 		var f = Random.Range(min, max);
 
 		if (Random.Range(0, 2) == 1)
