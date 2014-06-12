92db684730a6e9b678e9cc938f3fe6546588dd09
IceFlameCode <jalokim1998@poczta.onet.pl>
2014-06-12 20:12:57 +0200

    Added mobs
    Very simple & basic. Need to be controlled by CcNet and spawned properly
    (right now just added one on the Conflict Room to see how it works out).

--- Assets/Resources/Mob/AI/Standard/StandardAI.cs
+++ Assets/Resources/Mob/AI/Standard/StandardAI.cs
@@ -4,27 +4,38 @@ using System.Collections;
 public class StandardAI : MonoBehaviour {
 
 	[HideInInspector]
-	public Vector3 DesiredPos;
-	public float MaxJumpHeight = 5f;
-	public float MovementSpeed = 2f;
-	public float BodyHeight = 0.5f;
-	public float BodyWidth = 0.5f;
+	public Vector3 TargetPos; // the position of the player targeted by the AI
+	[HideInInspector]
+	public Vector3 DesiredPos; // next path node that the AI is heading towards
+	public float MovementSpeed = 5f;
 	public float Health = 1f;
-	public float Gravity = 10f;
 
-	Vector3 jumpVec;
+	GameObject player;
+	bool prevVisible = true;
 	
 	void Start () {
-		jumpVec = new Vector3(0f, MaxJumpHeight, 0f);
 	}
 
 	void Update () {
-		Vector3 ap = transform.position + Vector3.Normalize(DesiredPos - transform.position) * MovementSpeed * Time.deltaTime; // attempted pos
-		RaycastHit hit;
-		if (Physics.Raycast(transform.position, ap - transform.position, out hit, Vector3.Magnitude(ap - transform.position) + BodyWidth, 1<<0)) {
-			transform.position = hit.point - Vector3.Normalize(ap - transform.position) * BodyWidth;
+		player = GameObject.Find("FPSEntity(Clone)"); // put here for debug reasons
+		TargetPos = player.transform.position;
+		if (Vector3.SqrMagnitude(TargetPos - transform.position) < 10000f) {
+			if (Physics.Raycast(transform.position, (TargetPos - transform.position).normalized, Vector3.Distance(transform.position, TargetPos), 1<<0)) {
+				if (prevVisible) {
+					DesiredPos += (DesiredPos - TargetPos).normalized * 2f;
+					prevVisible = false;
+				}
+			} else {
+				DesiredPos = TargetPos;
+				prevVisible = true;
+			}
 		} else {
-			transform.position = ap;
+			DesiredPos = new Vector3(0f, 0f, 0f);
+			prevVisible = true;
+		}
+		if (Health > 0f) {
+			rigidbody.velocity = Vector3.Lerp(rigidbody.velocity, (DesiredPos - transform.position).normalized * MovementSpeed, Time.deltaTime);
+			transform.forward = Camera.main.transform.forward;
 		}
 	}
 }
